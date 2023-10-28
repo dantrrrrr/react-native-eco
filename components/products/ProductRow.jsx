8b9643e4058b5ee949b88ace7d05debaaab8a68d
dantr <huynhngdantr@gmail.com>
2023-10-28 15:55:44 +0700

    add custom hooks for fecth data and use it

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -1,18 +1,32 @@
-import { FlatList, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 import React from "react";
 import styles from "./productRow.style";
-import { SIZES } from "../../constants";
+import { COLORS, SIZES } from "../../constants";
 import ProductCartView from "./ProductCartView";
+import useFetch from "../../hook/useFetch";
 const ProductRow = () => {
-  const products = [1, 2, 3, 4];
+  const { data, error, isLoading, refetch } = useFetch();
+  console.log(
+    "🚀 ~ file: ProductRow.jsx:9 ~ ProductRow ~ isLoading:",
+    isLoading
+  );
+  console.log("🚀 ~ file: ProductRow.jsx:9 ~ ProductRow ~ error:", error);
+  console.log("🚀 ~ file: ProductRow.jsx:9 ~ ProductRow ~ data:", data.length);
   return (
     <View style={styles.container}>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductCartView />}
-        horizontal
-        contentContainerStyle={{ columnGap: SIZES.medium }}
-      />
+      {isLoading ? (
+        <ActivityIndicator size={SIZES.large} color={COLORS.primary} />
+      ) : error ? (
+        <Text>Something wentWrong</Text>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item }) => <ProductCartView />}
+          horizontal
+          contentContainerStyle={{ columnGap: SIZES.medium }}
+        />
+      )}
     </View>
   );
 };
